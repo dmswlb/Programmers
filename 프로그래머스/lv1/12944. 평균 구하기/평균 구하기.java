d04a94b5c9a16ad438e17ab01b8fa3b4975c3b15
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-09-16 13:46:03 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int data: arr){
+            answer+=data;
+        }
+        return answer/arr.length;
+    }
+}
