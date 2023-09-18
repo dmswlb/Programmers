5820657a36f4ca0581055ec1bb818f6451063b84
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-09-18 22:10:33 +0900

    Update 문자열 내 p와 y의 개수.java

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -5,14 +5,9 @@ class Solution {
         int cnty=0;
         int cntp=0;
         for(int i=0; i<arr.length; i++){
-           if(arr[i].equals("y")||arr[i].equals("Y")) {
-				cnty++;
-			}
-			else if(arr[i].equals("p")||arr[i].equals("P")) {
-				cntp++;
-			}
-        }
-        
+           	if(arr[i].equals("y")||arr[i].equals("Y")) cnty++;
+		else if(arr[i].equals("p")||arr[i].equals("P")) cntp++;			
+        }        
         if(cnty!=cntp) answer=false;
         return answer;
     }
