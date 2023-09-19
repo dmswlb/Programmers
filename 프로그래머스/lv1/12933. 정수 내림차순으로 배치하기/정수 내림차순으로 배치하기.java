d65704b254dcad17fbc5500e33fa11248fe2bc05
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-09-19 20:23:05 +0900

    Update 정수 내림차순으로 배치하기.java

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -4,12 +4,12 @@ class Solution {
     public long solution(long n) {
         long answer = 0;
        	String s= String.valueOf(n);
-		String[] arrS = s.split("");
-		int a=1;
-		Arrays.sort(arrS, Collections.reverseOrder());
+	String[] arrS = s.split("");
+	int a=1;
+	Arrays.sort(arrS, Collections.reverseOrder());
         for(int i=arrS.length-1; i>=0; i--) {
-			answer+=(long)Integer.parseInt(arrS[i])*a;
-			a*=10;
+		answer+=(long)Integer.parseInt(arrS[i])*a;
+		a*=10;
 		}
         return answer;
     }
