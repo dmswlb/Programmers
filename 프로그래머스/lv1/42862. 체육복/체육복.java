a4326ca7838f0b59625412e11e1d6e20e3bcd973
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-09-23 17:20:39 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int stdCnt = 0;
+        int[] std = new int[n];
+        
+        for(int l : lost) std[l-1] -=1;
+        for(int r : reserve) std[r-1] +=1;
+        
+        for(int i=0; i<n-1; i++){
+            if(std[i]+std[i+1]==0) {
+                std[i]=0;
+                std[i+1]=0;                
+            }
+        }
+        
+        for(int s:std){
+            if(s>=0) stdCnt++;
+        }
+        return stdCnt;
+    }
+}
