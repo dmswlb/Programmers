c658a7876d5acf79384ee8be9033839b16608d8d
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-09-29 21:13:15 +0900

    [unrated] Title: 둘만의 암호, Time: 0.86 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        int cnt=0;
+        char[] arr = skip.toCharArray();
+        Arrays.sort(arr);
+        
+        for(int i=97; i<123; i++){
+            if(arr[cnt]==(char)i){
+                if(cnt < arr.length -1) cnt++;
+                continue;
+            }
+            sb.append((char)i);
+        }
+        
+        for(int i=0; i< s.length(); i++){
+            for(int j=0; j<sb.length(); j++){
+                if(s.charAt(i) == sb.charAt(j)){
+                    if(j+index <sb.length()) answer.append(sb.charAt(j+index));
+                    else answer.append(sb.charAt((j- sb.length()+ index)%sb.length()));
+                }
+            }
+        }      
+        return answer.toString();
+    }
+}
