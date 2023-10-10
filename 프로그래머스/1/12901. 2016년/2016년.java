03bcceddd3d605ac42d2ba7c6b68a88f9cc3ccd4
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-10-11 08:45:23 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -4,23 +4,10 @@ class Solution {
         		//1월 1일이 금요일이기 때문
 		String[] w= {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
 		//5월 24일은 화요일임
-		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		int[] arr = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		int sum=0;
-		hm.put(1, 31);
-		hm.put(2, 29);
-		hm.put(3, 31);
-		hm.put(4, 30);
-		hm.put(5, 31);
-		hm.put(6, 30);
-		hm.put(7, 31);
-		hm.put(8, 31);
-		hm.put(9, 30);
-		hm.put(10, 31);
-		hm.put(11, 30);
-		hm.put(12, 31);		
-		
-		for(int i=1; i<a; i++) {
-			sum += hm.get(i);
+		for(int i=0; i<a-1; i++) {
+			sum += arr[i];
 		}		
 		sum+=b;		
 		return w[sum%7];	
