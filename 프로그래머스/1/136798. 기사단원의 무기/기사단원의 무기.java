a469869735295aa46e077b64bc32bdfae2812e7b
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-10-16 14:52:08 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.57 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,17 +1,18 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer =1;
-	int cnt=0;
+		int cnt=0;
 		
-	for(int i=2; i<=number; i++) {
-		for(int j=1; j<=Math.sqrt(i); j++) {
-			if(j*j==i) cnt++;
-                	else if(i%j==0) cnt+=2;
+		for(int i=2; i<=number; i++) {
+			for(int j=1; j<=Math.sqrt(i); j++) {
+				if(j*j==i) cnt++;
+                else if(i%j==0) cnt+=2;
+			}
+			if(cnt > limit) answer+=power;
+            else answer+=cnt;
+			cnt=0;
 		}
-		if(cnt > limit) answer+=power;
-            	else answer+=cnt;
-		cnt=0;
-	}		
+		
         return answer;
     }
 }
