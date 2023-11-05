c0e96eba1b00fc0084bfa470d582b9a1a3951cdf
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-11-05 16:08:07 +0900

    [level 2] Title: 구명보트, Time: 22.93 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int r =0;
+        int l = people.length-1;
+		int answer =0;        
+		while(r<=l){     
+            if(people[r]+people[r+1] >limit){
+                answer+=l-r+1;
+                break;
+            }
+			if(people[r]+people[l]<=limit){
+				r++;
+			}  
+            l--;
+            answer++;
+			if(r==l) {
+				answer++;
+				break;
+			}             
+		}
+		return answer;
+    }
+}
